0ba691c05e1ce769d11f61818cc9b4951af6708f
Daniel Micay <danielmicay@gmail.com>
2014-08-04 23:00:00 -0400

    rm a bunch of error handling boilerplate

--- playpen.c
+++ playpen.c
@@ -33,10 +33,17 @@ static void check(int rc) {
     if (rc < 0) errx(EXIT_FAILURE, "%s", strerror(-rc));
 }
 
+__attribute__((format(printf, 2, 3))) static void check_posix(intmax_t rc, const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    if (rc == -1) verr(EXIT_FAILURE, fmt, args);
+    va_end(args);
+}
+
 static void mountx(const char *source, const char *target, const char *filesystemtype,
                    unsigned long mountflags, const void *data) {
-    if (mount(source, target, filesystemtype, mountflags, data) < 0)
-        err(EXIT_FAILURE, "mounting %s failed", target);
+    check_posix(mount(source, target, filesystemtype, mountflags, data),
+                "mounting %s failed", target);
 }
 
 static const char *const systemd_bus_name = "org.freedesktop.systemd1";
@@ -115,8 +122,7 @@ static void stop_scope_unit(GDBusConnection *connection, const char *unit_name) 
 
 static void epoll_watch(int epoll_fd, int fd) {
     struct epoll_event event = { .data.fd = fd, .events = EPOLLIN };
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &event) < 0)
-        err(EXIT_FAILURE, "epoll_ctl");
+    check_posix(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &event), "epoll_ctl");
 }
 
 // This could often use `splice`, but it will not always work with `stdout` and `stderr`.
@@ -129,8 +135,7 @@ static void copy_pipe_to(int in_fd, int out_fd) {
             if (errno == EAGAIN) return;
             err(EXIT_FAILURE, "read");
         }
-        if (write(out_fd, buffer, (size_t)n) == -1)
-            err(EXIT_FAILURE, "write");
+        check_posix(write(out_fd, buffer, (size_t)n), "write");
     } while (n != 0);
 }
 
@@ -163,9 +168,8 @@ _Noreturn static void usage(FILE *out) {
 
 static void set_non_blocking(int fd) {
     int flags = fcntl(fd, F_GETFL, 0);
-    if (flags == -1) err(EXIT_FAILURE, "fcntl");
-    if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1)
-        err(EXIT_FAILURE, "fcntl");
+    check_posix(flags, "fcntl");
+    check_posix(fcntl(fd, F_SETFL, flags | O_NONBLOCK), "fcntl");
 }
 
 // Mark any extra file descriptors `CLOEXEC`. Only `stdin`, `stdout` and `stderr` are left open.
@@ -177,7 +181,7 @@ static void prevent_leaked_file_descriptors() {
         char *end;
         int fd = (int)strtol(dp->d_name, &end, 10);
         if (*end == '\0' && fd > 2 && fd != dirfd(dir)) {
-            if (ioctl(fd, FIOCLEX) == -1) err(EXIT_FAILURE, "ioctl");
+            check_posix(ioctl(fd, FIOCLEX), "ioctl");
         }
     }
     closedir(dir);
@@ -191,12 +195,6 @@ static long strtolx_positive(const char *s, const char *what) {
     if (*end != '\0' || result < 0)
         errx(EXIT_FAILURE, "%s must be a positive integer", what);
     return result;
-}
-
-static void pipex(int pipefd[2]) {
-    if (pipe(pipefd) < 0) {
-        err(EXIT_FAILURE, "pipe");
-    }
 }
 
 int main(int argc, char **argv) {
@@ -295,56 +293,47 @@ int main(int argc, char **argv) {
     }
 
     int epoll_fd = epoll_create1(EPOLL_CLOEXEC);
-    if (epoll_fd < 0) {
-        err(EXIT_FAILURE, "epoll");
-    }
+    check_posix(epoll_fd, "epoll_create1");
 
     sigset_t mask;
     sigemptyset(&mask);
     sigaddset(&mask, SIGCHLD);
 
-    if (sigprocmask(SIG_BLOCK, &mask, NULL) < 0) {
-        err(EXIT_FAILURE, "sigprocmask");
-    }
+    check_posix(sigprocmask(SIG_BLOCK, &mask, NULL), "sigprocmask");
 
     epoll_watch(epoll_fd, STDIN_FILENO);
 
     int sig_fd = signalfd(-1, &mask, SFD_CLOEXEC);
-    if (sig_fd < 0) {
-        err(EXIT_FAILURE, "signalfd");
-    }
+    check_posix(sig_fd, "signalfd");
 
     epoll_watch(epoll_fd, sig_fd);
 
     int timer_fd = -1;
     if (timeout) {
         timer_fd = timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK|TFD_CLOEXEC);
-        if (timer_fd < 0)
-            err(EXIT_FAILURE, "timerfd_create");
-
+        check_posix(timer_fd, "timerfd_create");
         epoll_watch(epoll_fd, timer_fd);
     }
 
     int pipe_in[2];
     int pipe_out[2];
     int pipe_err[2];
-    pipex(pipe_in);
-    pipex(pipe_out);
+    check_posix(pipe(pipe_in), "pipe");
+    check_posix(pipe(pipe_out), "pipe");
     set_non_blocking(pipe_out[0]);
-    pipex(pipe_err);
+    check_posix(pipe(pipe_err), "pipe");
     set_non_blocking(pipe_err[0]);
 
     // A pipe for signalling that the scope unit is set up.
     int pipe_ready[2];
-    if (pipe2(pipe_ready, O_CLOEXEC) < 0) {
-        err(EXIT_FAILURE, "pipe");
-    }
+    check_posix(pipe2(pipe_ready, O_CLOEXEC), "pipe2");
 
     epoll_watch(epoll_fd, pipe_out[0]);
     epoll_watch(epoll_fd, pipe_err[0]);
 
     unsigned long flags = SIGCHLD|CLONE_NEWIPC|CLONE_NEWNS|CLONE_NEWPID|CLONE_NEWUTS|CLONE_NEWNET;
     pid_t pid = (pid_t)syscall(__NR_clone, flags, NULL);
+    check_posix(pid, "clone");
 
     if (pid == 0) {
         dup2(pipe_in[0], STDIN_FILENO);
@@ -369,14 +358,10 @@ int main(int argc, char **argv) {
         // Wait until the scope unit is set up before moving on. This also ensures that the parent
         // didn't die before `prctl` was called.
         uint8_t ready;
-        if (read(pipe_ready[0], &ready, sizeof ready) == -1) {
-            err(EXIT_FAILURE, "read");
-        }
+        check_posix(read(pipe_ready[0], &ready, sizeof ready), "read");
         close(pipe_ready[0]);
 
-        if (sethostname(hostname, strlen(hostname)) < 0) {
-            err(EXIT_FAILURE, "sethostname");
-        }
+        check_posix(sethostname(hostname, strlen(hostname)), "sethostname");
 
         // avoid propagating mounts to or from the real root
         mountx(NULL, "/", NULL, MS_PRIVATE|MS_REC, NULL);
@@ -387,9 +372,8 @@ int main(int argc, char **argv) {
         // re-mount as read-only
         mountx(root, root, "bind", MS_BIND|MS_REMOUNT|MS_RDONLY|MS_REC, NULL);
 
-        if (chroot(root) < 0 || chdir("/") < 0) {
-            err(EXIT_FAILURE, "entering chroot `%s` failed", root);
-        }
+        check_posix(chroot(root), "chroot into `%s` failed", root);
+        check_posix(chdir("/"), "entering chroot `%s` failed", root);
 
         if (mount_proc) {
             mountx(NULL, "/proc", "proc", MS_NOSUID|MS_NOEXEC|MS_NODEV, NULL);
@@ -413,24 +397,14 @@ int main(int argc, char **argv) {
         mountx(NULL, pw->pw_dir, "tmpfs", MS_NOSUID|MS_NODEV, NULL);
 
         // switch to the user's home directory as a login shell would
-        if (chdir(pw->pw_dir)) {
-            err(EXIT_FAILURE, "chdir");
-        }
+        check_posix(chdir(pw->pw_dir), "chdir");
 
         // create a new session
-        if (setsid() < 0) {
-            err(EXIT_FAILURE, "setsid");
-        }
+        check_posix(setsid(), "setsid");
 
-        if (initgroups(username, pw->pw_gid) < 0) {
-            err(EXIT_FAILURE, "initgroups");
-        }
-        if (setresgid(pw->pw_gid, pw->pw_gid, pw->pw_gid) < 0) {
-            err(EXIT_FAILURE, "setresgid");
-        }
-        if (setresuid(pw->pw_uid, pw->pw_uid, pw->pw_uid) < 0) {
-            err(EXIT_FAILURE, "setresuid");
-        }
+        check_posix(initgroups(username, pw->pw_gid), "initgroups");
+        check_posix(setresgid(pw->pw_gid, pw->pw_gid, pw->pw_gid), "setresgid");
+        check_posix(setresuid(pw->pw_uid, pw->pw_uid, pw->pw_uid), "setresuid");
 
         char path[] = "PATH=/usr/local/bin:/usr/bin:/bin";
         char *env[] = {path, NULL, NULL, NULL, NULL};
@@ -459,12 +433,7 @@ int main(int argc, char **argv) {
         }
 
         check(seccomp_load(ctx));
-
-        if (execvpe(argv[optind], argv + optind, env) < 0) {
-            err(EXIT_FAILURE, "execvpe");
-        }
-    } else if (pid < 0) {
-        err(EXIT_FAILURE, "clone");
+        check_posix(execvpe(argv[optind], argv + optind, env), "execvpe");
     }
 
     GDBusConnection *connection = get_system_bus();
@@ -474,14 +443,11 @@ int main(int argc, char **argv) {
 
     start_scope_unit(connection, pid, memory_limit, devices, unit_name);
 
-    if (write(pipe_ready[1], &(uint8_t) { 0 }, 1) == -1) {
-        err(EXIT_FAILURE, "write");
-    }
+    check_posix(write(pipe_ready[1], &(uint8_t) { 0 }, 1), "write");
 
     if (timeout) {
         struct itimerspec spec = { .it_value = { .tv_sec = timeout } };
-        if (timerfd_settime(timer_fd, 0, &spec, NULL) < 0)
-            err(EXIT_FAILURE, "timerfd_settime");
+        check_posix(timerfd_settime(timer_fd, 0, &spec, NULL), "timerfd_settime");
     }
 
     struct epoll_event events[4];
@@ -511,10 +477,9 @@ int main(int argc, char **argv) {
                 } else if (evt->data.fd == sig_fd) {
                     struct signalfd_siginfo si;
                     ssize_t bytes_r = read(sig_fd, &si, sizeof(si));
+                    check_posix(bytes_r, "read");
 
-                    if (bytes_r < 0) {
-                        err(EXIT_FAILURE, "read");
-                    } else if (bytes_r != sizeof(si)) {
+                    if (bytes_r != sizeof(si)) {
                         errx(EXIT_FAILURE, "read the wrong amount of bytes");
                     } else if (si.ssi_signo != SIGCHLD) {
                         errx(EXIT_FAILURE, "got an unexpected signal");
@@ -542,9 +507,7 @@ int main(int argc, char **argv) {
                 } else if (evt->data.fd == STDIN_FILENO) {
                     uint8_t buffer[BUFSIZ];
                     ssize_t n = read(STDIN_FILENO, buffer, sizeof buffer);
-                    if (n == -1) {
-                        err(EXIT_FAILURE, "read");
-                    }
+                    check_posix(n, "read");
                     if (n == 0) {
                         epoll_ctl(epoll_fd, EPOLL_CTL_DEL, STDIN_FILENO, NULL);
                         close(STDIN_FILENO);
